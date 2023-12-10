0b8a439295ae28dd0836fe0bebcf4a67757f8c2f
nayeemuu <nayeem.uucse@gmail.com>
2023-12-10 21:16:51 +0600

    private route issue solved

--- src/app/(privateRoutes)/create-article/page.js
+++ src/app/(privateRoutes)/create-article/page.js
@@ -1,18 +1,24 @@
-import CreateArticles from "@/Components/CreateArticles/CreateArticles";
-import { cookies } from "next/headers";
+import CreateArticles from '@/Components/CreateArticles/CreateArticles';
+import PrivateRoute from '@/helpers/PrivateRoute';
+import { cookies } from 'next/headers';
 
 export const metadata = {
-  title: "Create Article",
-  description: "This is Create Article page",
+  title: 'Create Article',
+  description: 'This is Create Article page',
 };
 
 export default function CreateArticle() {
   const cookieStore = cookies();
-  const access_token = cookieStore.get("access_token");
+  const access_token = cookieStore.get('access_token');
   // console.log('access_token = ', access_token);
-  return access_token?.value ? (
-    <CreateArticles />
-  ) : (
-    <div className="container">you do not have access</div>
+  return (
+    <>
+      <PrivateRoute />
+      {access_token?.value ? (
+        <CreateArticles />
+      ) : (
+        <div className="container">you do not have access</div>
+      )}
+    </>
   );
 }
--- src/app/(privateRoutes)/my-profile/page.js
+++ src/app/(privateRoutes)/my-profile/page.js
@@ -1,26 +1,28 @@
-import { cookies } from "next/headers";
-import PrivateRoute from "@/helpers/PrivateRoute";
-import MyProfile from "./MyProfile";
+import { cookies } from 'next/headers';
+import PrivateRoute from '@/helpers/PrivateRoute';
+import MyProfile from './MyProfile';
 
 export const metadata = {
-  title: "My Profile",
-  description: "This is MyProfile page",
+  title: 'My Profile',
+  description: 'This is MyProfile page',
 };
 
 export default function Home() {
   const cookieStore = cookies();
-  const access_token = cookieStore.get("access_token");
+  const access_token = cookieStore.get('access_token');
   // console.log('access_token = ', access_token.value);
 
-  return access_token?.value ? (
+  return (
     <>
-      <div className="container">
-        <div>Your Profile</div>
-        <MyProfile></MyProfile>
-        <PrivateRoute />
-      </div>
+      <PrivateRoute />
+      {access_token?.value ? (
+        <div className="container">
+          <div>Your Profile</div>
+          <MyProfile />
+        </div>
+      ) : (
+        <div className="container">you do not have access</div>
+      )}
     </>
-  ) : (
-    <div className="container">you do not have access</div>
   );
 }
